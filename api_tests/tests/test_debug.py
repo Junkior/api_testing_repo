45f4910bd9ef05d783a95650fe7623529e377cff
Junkior <punkissimple@gmail.com>
2023-04-20 23:27:02 +0300

    update first test|add pytest logging

--- api_tests/tests/test_debug.py
+++ api_tests/tests/test_debug.py
@@ -1,9 +1,20 @@
+import requests.exceptions
+
 from api_tests.api_pages.main_api_page import StartPage
 from api_tests.Schemas.start_page import valid_schema
 
 URL = 'https://www.dnd5eapi.co'
 
-def test_demo():
-    response = StartPage(url=URL).get_start_page(json_schema=valid_schema)
-    print(response.text)
-    assert response.status_code==200
+def test_start_api_page():
+    try:
+        response = StartPage(url=URL).get_start_page(json_schema=valid_schema)
+        response.raise_for_status()
+        body = response.json()
+    except requests.exceptions.HTTPError as err:
+        print("HTTP error:", err)
+    except ValueError as err:
+        print("Json parse error:", err)
+
+    else:
+        assert response.status_code == 200
+        assert body["rules"] == "/api/rules"
