9aa9a5be677efe95eb1913e037e94d4f70c1b007
Junkior <punkissimple@gmail.com>
2023-04-23 19:09:28 +0300

    add charisma ability score endpoint

--- api_tests/Schemas/ability_scores_schemas.py
+++ api_tests/Schemas/ability_scores_schemas.py
@@ -129,3 +129,121 @@ ability_scores_schema = {
     "results"
   ]
 }
+
+ability_scores_charisma_schema = {
+"type": "object",
+  "properties": {
+    "index": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    },
+    "full_name": {
+      "type": "string"
+    },
+    "desc": {
+      "type": "array",
+      "items": [
+        {
+          "type": "string"
+        },
+        {
+          "type": "string"
+        }
+      ]
+    },
+    "skills": {
+      "type": "array",
+      "items": [
+        {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "index": {
+              "type": "string"
+            },
+            "url": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "name",
+            "index",
+            "url"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "index": {
+              "type": "string"
+            },
+            "url": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "name",
+            "index",
+            "url"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "index": {
+              "type": "string"
+            },
+            "url": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "name",
+            "index",
+            "url"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "index": {
+              "type": "string"
+            },
+            "url": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "name",
+            "index",
+            "url"
+          ]
+        }
+      ]
+    },
+    "url": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "index",
+    "name",
+    "full_name",
+    "desc",
+    "skills",
+    "url"
+  ]
+}
--- api_tests/api_pages/ability_scores.py
+++ api_tests/api_pages/ability_scores.py
@@ -18,3 +18,20 @@ class AbilityScores:
         response = self.client.custom_request("GET", f"{self.url}{self.ABILITY_SCORES}")
         validate(instance=response.json(), schema=json_schema)
         return response
+
+class AbilityScoresCharisma:
+    def __init__(self, url):
+        self.url = url
+        self.client = Client
+
+    CHARISMA = 'api/ability-scores/cha'
+    def get_charisma_ability_scores(self, json_schema):
+        """
+        Get charisma ability score
+        :param json_schema: json_schema: schema for validate
+        :return: response of request
+        """
+        response = self.client.custom_request("GET", f"{self.url}{self.CHARISMA}")
+        validate(instance=response.json(), schema=json_schema)
+        return response
+
